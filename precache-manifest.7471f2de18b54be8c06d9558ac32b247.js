5c605727c82d84f4347a242a9d4605bb2045c34e
Vishakh Dadwal <vishakhdadwal999@gmail.com>
2019-12-09 14:22:53 +0530

    Updates

---
+++ precache-manifest.7471f2de18b54be8c06d9558ac32b247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17af393ae6246dbb6aed073ef0361871",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "4f1b570db4a502bc0bc2",
+    "url": "/test-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9386138d766d2bbede8",
+    "url": "/test-react/static/js/2.9747f88b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-react/static/js/2.9747f88b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f1b570db4a502bc0bc2",
+    "url": "/test-react/static/js/main.83bc2387.chunk.js"
+  },
+  {
+    "revision": "a9ae7c3805d46f9a4e12",
+    "url": "/test-react/static/js/runtime-main.4507aa62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
